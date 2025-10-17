43f40f781b9216e53ae246a92425d95fbface472
HaiHui <154984842+HaiHui886@users.noreply.github.com>
2025-10-17 13:36:48 +0800

    feat(app): update main entry point to use app (#20)
    - Changed import from main to app
    - Updated protocol to 'sse' in both files
    
    Co-authored-by: Haihui.Wang <wanghh2000@163.com>

--- evaluation/app.py
+++ evaluation/app.py
@@ -1,11 +1,10 @@
 import sys
 import os
 
-from src.csghub_mcp_server_evaluation.main import main
+from src.csghub_mcp_server_evaluation.main import app
 
 if __name__ == "__main__":
     # streamable-http
     # app(host='0.0.0.0', port=8000, protocol='streamable-http')
     # sse
-    # app(host='0.0.0.0', port=8000, protocol='sse')
-    main()
+    app(host='0.0.0.0', port=8000, protocol='sse')
--- space/app.py
+++ space/app.py
@@ -1,5 +1,5 @@
 import logging
-from src.csghub_mcp_server_space.main import main
+from src.csghub_mcp_server_space.main import app
 
 logger = logging.getLogger(__name__)
 
@@ -7,5 +7,4 @@ if __name__ == "__main__":
     # streamable-http
     # app(host='0.0.0.0', port=8000, protocol='streamable-http')
     # sse
-    # app(host='0.0.0.0', port=8000, protocol='stdio')
-    main()
+    app(host='0.0.0.0', port=8000, protocol='sse')
