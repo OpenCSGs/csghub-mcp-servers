66442b839e30acc746a64d6cd195450301706996
Haihui.Wang <wanghh2000@163.com>
2025-10-18 16:42:18 +0800

    Add query_my_spaces function
    - Implement query_my_spaces API call
    - Add list_my_spaces tool registration
    - Update imports in api_client and space modules

--- space/src/csghub_mcp_server_space/api_client/__init__.py
+++ space/src/csghub_mcp_server_space/api_client/__init__.py
@@ -1,5 +1,6 @@
 from .user import api_get_username_from_token
 from .space import api_get_top_download_spaces, create, start
+from .space import query_my_spaces
 from .repo import upload_file
 from .space_resources import get_space_resources
 from .cluster import get_clusters
