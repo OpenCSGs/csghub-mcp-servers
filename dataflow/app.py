71d0c3c4251253e1869dceb9cbd125041f288a4d
HaiHui <154984842+HaiHui886@users.noreply.github.com>
2025-10-28 22:27:58 +0800

    Add csghub dataflow mcp tools (#36)
    * Remove redundant error handling
    
    - Commented out 500 status code handling
    - Simplified error logging logic
    
    * feat(dataflow): initial implementation of CSGHub MCP tools
    
    - add .gitignore for Python project
    - create README.md with installation instructions
    - implement main application logic
    - add API client for CSGHub interactions
    - register dataflow tools with FastMCP
    - define argument parsing for server configuration
    - include version management in setup
    - establish logging configuration and signal handling
    
    ---------
    
    Co-authored-by: Haihui.Wang <wanghh2000@163.com>

---
+++ dataflow/app.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+from src.csghub_mcp_server_dataflow.main import app
+
+if __name__ == "__main__":
+    # streamable-http
+    # app(host='0.0.0.0', port=8000, protocol='streamable-http')
+    # sse
+    app(host='0.0.0.0', port=8000, protocol='sse')
---
+++ dataflow/src/csghub_mcp_server_dataflow/api_client/__init__.py
@@ -0,0 +1,7 @@
+from .user import api_get_username_from_token
+from .dataflow import api_list_jobs
+from .dataflow import api_get_job_details
+from .dataflow import api_create_job
+from .dataflow import api_delete_job
+from .dataflow import api_get_template_list
+from .dataset import api_get_dataset_detail
