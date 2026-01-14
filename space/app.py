d3d920420377ab90773d5af8a786513ccd439b6d
HaiHui <154984842+HaiHui886@users.noreply.github.com>
2026-01-14 11:05:20 +0800

    Enable streamable-http protocol (#72)
    - Enable streamable-http in all apps
    - Disable sse protocol in all apps
    
    Co-authored-by: Haihui.Wang <wanghh2000@163.com>

--- space/app.py
+++ space/app.py
@@ -5,6 +5,6 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     # streamable-http
-    # app(host='0.0.0.0', port=8000, protocol='streamable-http')
+    app(host='0.0.0.0', port=8000, protocol='streamable-http')
     # sse
-    app(host='0.0.0.0', port=8000, protocol='sse')
+    # app(host='0.0.0.0', port=8000, protocol='sse')
